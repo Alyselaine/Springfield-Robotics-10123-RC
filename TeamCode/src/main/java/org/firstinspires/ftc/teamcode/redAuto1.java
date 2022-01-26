b263b2e884f0bc556d63e626b1aa1ccb84043e2a
Springfield Robotics #8856 and #10123 <34609805+roboticsGHS@users.noreply.github.com>
2022-01-26 16:05:15 -0600

    Started red and blue autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redAuto1.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class redAuto1 extends LinearOpMode {
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor duckSpinRight;
+    private DcMotor duckSpinLeft;
+    private DcMotor liftey;
+    private CRServo pickupLeft;
+    private CRServo pickupRight;
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+
+
+        // Drive Motors
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+        // Object Movement
+        duckSpinRight = hardwareMap.get(DcMotor.class, "duckSpinRight");
+        duckSpinLeft = hardwareMap.get(DcMotor.class, "duckSpinLeft");
+        liftey = hardwareMap.get(DcMotor.class, "liftey");
+        pickupLeft = hardwareMap.get(CRServo.class, "pickupLeft");
+        pickupRight = hardwareMap.get(CRServo.class, "pickupRight");
+
+        // Reverse one of the drive motors.
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+        if (opModeIsActive()) {
+            blockLift(1);
+            sleep(300);
+            drive(-1);
+            sleep(450);
+            brake();
+        }
+
+    }
+
+
+    private void brake () {
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+    }
+
+    private void turn ( double power){
+        frontLeft.setPower(power);
+        backLeft.setPower(power);
+        frontRight.setPower(-power);
+        backRight.setPower(-power);
+    }
+
+    //strafe the robot
+    private void strafe ( double power){
+        frontLeft.setPower(power);
+        backLeft.setPower(-power);
+        frontRight.setPower(-power);
+        backRight.setPower(power);
+    }
+
+    //drive robot forward or backwards
+    private void drive(double power) {
+        frontLeft.setPower(-power);
+        backLeft.setPower(-power);
+        frontRight.setPower(-power);
+        backRight.setPower(-power);
+    }
+
+    // Spins the block pickup wheels
+    private void itemPickup(double power) {
+        pickupRight.setPower(-power);
+        pickupLeft.setPower(power);
+    }
+
+    // block lift mechanism
+    private void blockLift(double power) {
+        liftey.setPower(power);
+    }
+}
+
+
